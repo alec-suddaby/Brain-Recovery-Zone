01db6c0fa4ca8112745bbe9fd61a866c44287332
Jack Churchill <jack@visualise.com>
2020-07-31 17:03:05 +0100

    0.0.3
    - Unity 2019.4.5f1
    - 0.0.3
    - Edited the Menu Manager script to remove things that aren't needed and update the OVR ref to XR Toolkit
    - Made a working menu system to click through to each sub menu and back
    - Added a hide and show button for the menu, although this is buggy and sometimes these buttons disappear. Best to fix through script rather than current means of set active in the on click functions
    - Added a back button which hides when at the top menu
    - Filled out placeholder menu items across the three sub menus
    - Added particles to the menu in 3d space

--- 670_KDL_MVPA/Assets/Scripts/MenuSystem/MenuManager.cs
+++ 670_KDL_MVPA/Assets/Scripts/MenuSystem/MenuManager.cs
@@ -1,38 +1,53 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
-using UnityEngine.XR.Interaction.Toolkit;
+using UnityEngine.XR;
 
 public class MenuManager : MonoBehaviour
 {
     public Panel currentPanel = null;
-    public GameObject carInteriors;
 
     private List<GameObject> panelPopupBox = new List<GameObject>();
-    private Canvas panelPopupBoxCanvas;
-    private bool panelPopupBoxCanvasBool;
-    // When this turns to true it will trigger the background to be removed
-    private bool panelPopupBoxCanvasTriggerBackground = true;
 
     private List<Panel> panelHistory = new List<Panel>();
 
-    public AudioClip backButtonPress;
-    public AudioClip buttonDownSound;
-
-    private AudioSource backAudioSrc;
-
     private Button[] allButtons;
 
-    private void Start()
+    //XR Toolkit controllers
+    [SerializeField]
+    private XRNode xRNode = XRNode.LeftHand;
+
+    private List<InputDevice> devices = new List<InputDevice>();
+
+    private InputDevice device;
+
+    //to avoid repeat readings
+    private bool secondaryButtonIsPressed;
+
+
+    void GetDevice()
     {
-        SetupPanels();
-        SetupCarInteriors();
-        backAudioSrc = GetComponent<AudioSource>();
+        InputDevices.GetDevicesAtXRNode(xRNode, devices);
+        device = devices.FirstOrDefault();
     }
 
-    private void SetupPanels()
+    void OnEnable()
+    {
+        if(!device.isValid)
+        {
+            GetDevice();
+        }    
+    }
+
+    void Start()
+    {
+        SetupPanels();
+    }
+
+    void SetupPanels()
     {
         Panel[] panels = GetComponentsInChildren<Panel>();
 
@@ -42,42 +57,24 @@ public class MenuManager : MonoBehaviour
         currentPanel.Show();
     }
 
-    private void SetupCarInteriors()
+    void Update()
     {
-        //Debug.Log("Remove Car Interior");
-        // Sets each child of CarInteriors GameObject is active to false
-        for (int a = 0; a < carInteriors.transform.childCount; a++)
+        if(!device.isValid)
         {
-            carInteriors.transform.GetChild(a).gameObject.SetActive(false);
+            GetDevice();
         }
-        //Debug.Log("Bool panelPopupBoxCanvasTriggerBackground: " + panelPopupBoxCanvasTriggerBackground);
-    }
 
-    private void Update()
-    {
+        // capturing primary button press and release
+        bool secondaryButtonValue = false;
+        InputFeatureUsage<bool> secondaryButtonUsage = CommonUsages.secondaryButton;
+        
         //if (OVRInput.GetDown(OVRInput.Button.Back) || OVRInput.GetDown(OVRInput.Button.Two))
-        if (InputDevice. || OVRInput.GetDown(OVRInput.Button.Two))
+        if (device.TryGetFeatureValue(secondaryButtonUsage, out secondaryButtonValue) && secondaryButtonValue && !secondaryButtonIsPressed)
         {
-            backAudioSrc.PlayOneShot(backButtonPress);
+            Debug.Log("####################PRESSSSEDDDDD");
             GoToPrevious();
         }
-
-        //trying to make the audio work for all buttons
-        /*
-        for (int i = 0; i < allButtons.Length; i++)
-        {
-            allButtons[].onClick;
-        }
-        */
-
     }
-
-    /*
-    public void TaskOnClick( int buttonIndex )
-    {
-        backAudioSrc.PlayOneShot(buttonDownSound);
-    }
-    */
 
     public void GoToPrevious()
     {
@@ -86,73 +83,29 @@ public class MenuManager : MonoBehaviour
         if(panelHistory.Count == 0)
         //execute function if there is no panel histroy, for example exiting the app? else..... return;
         {
-            OVRManager.PlatformUIConfirmQuit();
+            //OVRManager.PlatformUIConfirmQuit();
             return;
-        }
-        
-        //Finding if any of the Panel_PopupBox canvas are active
-        //CURRENTLY ONLY WORKS WITH ONE I THINK
-        //panelPopupBox.AddRange(GameObject.FindGameObjectsWithTag("Panel_PopupBox"));
-        panelPopupBox.AddRange(GameObject.FindGameObjectsWithTag("Panel_PopupBox"));
-
-        //Debug.Log("Amount of panel boxes in array " + panelPopupBox.Count);
-
-        if(panelPopupBox.Count > 0)
-        {  
-            for (int p = 0; p < panelPopupBox.Count; p++)
-            {
-                //Debug.Log("Loop Start");
-                panelPopupBoxCanvas = panelPopupBox[p].GetComponent<Canvas>();
-                panelPopupBoxCanvasBool = panelPopupBoxCanvas.enabled;
-                //Debug.Log("Checking against number: " + p + " = " + panelPopupBoxCanvasBool);
-
-                // If any Panel_PopupBox are active (true) then this script runs
-                if(panelPopupBoxCanvasBool == true)
-                {
-                    // Setting the panelPopupBoxCanvasTriggerBackground to false will stop the SetupCarInteriors script from running
-                    panelPopupBoxCanvasTriggerBackground = false;
-                }
-                //Debug.Log("Loop End"); 
-            }
-            //Debug.Log("Bool panelPopupBoxCanvasTriggerBackground: " + panelPopupBoxCanvasTriggerBackground);
-
-            // If a Panel_PopupBox is active and the panelPopupBoxCanvasTriggerBackground has been set to true then the car intereior will be removed by running SetupCarInteriors
-            if(panelPopupBoxCanvasTriggerBackground == true)
-            {
-                //Debug.Log("Run SetupCarInteriors();");
-                SetupCarInteriors();
-            }
         }
 
         int lastIndex = panelHistory.Count - 1;
         SetCurrent(panelHistory[lastIndex]);
         panelHistory.RemoveAt(lastIndex);
-        //Clear list of Panel_PopupBox
-        panelPopupBox.Clear();
-
-        // Reset the trigger to true
-        panelPopupBoxCanvasTriggerBackground = true;
-
-        //Debug.Log("End Back Press");
     }
  
     public void SetCurrentWithHistory(Panel newPanel)
     {
-        ClickSound();
         panelHistory.Add(currentPanel);
         SetCurrent(newPanel);
     }
 
     public void SetCurrentChildWithHistory(Panel newPanel)
     {
-        ClickSound();
         panelHistory.Add(currentPanel);
         SetCurrentChild(newPanel);
     }
 
     public void SetCurrent(Panel newPanel)
     {
-        ClickSound();
         currentPanel.Hide();
 
         currentPanel = newPanel;
@@ -161,28 +114,12 @@ public class MenuManager : MonoBehaviour
 
     public void SetCurrentChild(Panel newPanel)
     {
-        ClickSound();
         currentPanel = newPanel;
         currentPanel.Show();
     }
-
-    // Should no longer need this function as the Previous() function now handles if the interior should be removed
-    /*public void SetCurrentRemoveInteriors(Panel newPanel)
-    {
-        currentPanel.Hide();
-        SetupCarInteriors();
-
-        currentPanel = newPanel;
-        currentPanel.Show();
-    }*/
 
     public void LoadScene(string level)
     {
         SceneManager.LoadSceneAsync(level, LoadSceneMode.Additive);
-    }
-
-    public void ClickSound()
-    {
-        backAudioSrc.PlayOneShot(buttonDownSound);
     }
 }
---
+++ 670_KDL_MVPA/Assets/Scripts/MenuSystem/TopMenuToolsMenuRelationship.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TopMenuToolsMenuRelationship : MonoBehaviour
+{
+    private Canvas thisCanvas = null;
+
+    public GameObject backButton;
+    
+    private void Awake()
+    {
+        thisCanvas = GetComponent<Canvas>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       if(thisCanvas.isActiveAndEnabled == true)
+       {
+            backButton.SetActive(false);
+       }
+       else if (thisCanvas.isActiveAndEnabled == false)
+       {
+           backButton.SetActive(true);
+       }
+       else
+       {
+           backButton.SetActive(true);
+       }
+
+    }
+}
