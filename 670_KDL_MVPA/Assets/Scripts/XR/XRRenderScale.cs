2257b1854f91ca61eae2e98cc16c4b980a871fc3
Jack Churchill <jack@visualise.com>
2020-08-06 13:15:05 +0100

    0.0.9
    - Unity 2019.4.5f1
    - 0.0.9
    - Created the XRRenderScale script to upscale the app to x1.5 quality. Now everything looks amazing, if anything drops in performance, this will be why

---
+++ 670_KDL_MVPA/Assets/Scripts/XR/XRRenderScale.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEngine.XR;
+
+public class VRRenderScale : MonoBehaviour {
+    public float vrScale = 1.5f;
+
+    void Start () {
+        XRSettings.eyeTextureResolutionScale = vrScale;
+    }
+}
---
+++ 670_KDL_MVPA/Assets/XRRenderScale.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityEngine.XR;
+
+public class XRRenderScale : MonoBehaviour 
+{
+    public float vrScale = 1.5f;
+
+    void Start () {
+        XRSettings.eyeTextureResolutionScale = vrScale;
+    }
+}
