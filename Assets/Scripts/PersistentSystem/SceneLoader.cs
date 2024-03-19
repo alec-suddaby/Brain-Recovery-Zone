48c85f778e567f664be050db2deddb996143b37b
Suddaby <114822942+Suddaby@users.noreply.github.com>
2024-03-19 22:29:55 +0000

    Add Unit Test Folders

--- Assets/Scripts/PersistentSystem/SceneLoader.cs
+++ Assets/Scripts/PersistentSystem/SceneLoader.cs
@@ -1,8 +1,8 @@
 ﻿using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.SceneManagement;
-using System.Collections.Generic;
 
 public class SceneLoader : Singleton<SceneLoader>
 {
@@ -13,45 +13,45 @@ public class SceneLoader : Singleton<SceneLoader>
     public string mainMenuName;
     private string sceneName;
 
-    private bool isLoading = false;
+    public bool IsLoading { get; private set; } = false;
 
     // Deleted player prefs temp holding
     private string deletedSavedCurrentPanel;
     private int deletedSavedPanelHistoryCount;
-    private List<string> deletedSavedPanelHistory = new List<string>(); 
+    private List<string> deletedSavedPanelHistory = new List<string>();
     private int deleteShowPostLikert;
-    
-    
+
+
     private void Awake()
     {
-      if (!Application.isEditor)
-      {
+        if (!Application.isEditor)
+        {
             LoadPersistent();
-      }
+        }
 
-      StartCoroutine(StartupFade());
-      SceneManager.sceneLoaded += SetActiveScene;
+        StartCoroutine(StartupFade());
+        SceneManager.sceneLoaded += SetActiveScene;
     }
 
     private void OnDestroy()
     {
-      Debug.Log("Scene Loader Destroy");
-      SceneManager.sceneLoaded -= SetActiveScene;
+        Debug.Log("Scene Loader Destroy");
+        SceneManager.sceneLoaded -= SetActiveScene;
     }
 
     private void OnApplicationPause()
     {
-      Debug.Log("Application is paused and player prefs have been cleared");
-      ClearPlayerPrefPanelHistory();
+        Debug.Log("Application is paused and player prefs have been cleared");
+        ClearPlayerPrefPanelHistory();
     }
 
     private void OnApplicationFocus()
     {
-      Debug.Log("Application is running and player prefs have been reloaded");
-      ReloadPlayerPrefPanelHistory();
+        Debug.Log("Application is running and player prefs have been reloaded");
+        ReloadPlayerPrefPanelHistory();
     }
-    
-    private void LoadPersistent()
+
+    public void LoadPersistent()
     {
         SceneManager.LoadSceneAsync(mainMenuName, LoadSceneMode.Additive);
     }
@@ -60,75 +60,79 @@ public class SceneLoader : Singleton<SceneLoader>
     public void ReturnToMenu()
     {
         sceneName = mainMenuName;
-        if(!isLoading)
+        if (!IsLoading)
         {
-          StartCoroutine(LoadScene(sceneName));
+            StartCoroutine(LoadScene(sceneName));
         }
     }
 
     // sceneName is the string for the name of the new scene
     public void LoadNewScene(string sceneName)
     {
-        if(!isLoading)
+        if (!IsLoading)
         {
-          StartCoroutine(LoadScene(sceneName));
+            StartCoroutine(LoadScene(sceneName));
         }
     }
 
     private IEnumerator LoadScene(string sceneName)
     {
-      isLoading = true;
+        IsLoading = true;
 
-      //OnLoadBegin?.Invoke();
-      yield return screenFader.StartFadeIn();
-      yield return StartCoroutine(UnloadCurrent());
+        //OnLoadBegin?.Invoke();
+        yield return screenFader.StartFadeIn();
+        yield return StartCoroutine(UnloadCurrent());
 
-      // Fake wait time
-      //yield return new WaitForSeconds(1.0f);
+        // Fake wait time
+        //yield return new WaitForSeconds(1.0f);
 
-      yield return StartCoroutine(LoadNew(sceneName));
-      yield return screenFader.StartFadeOut();
-      //OnLoadEnd?.Invoke();
+        yield return StartCoroutine(LoadNew(sceneName));
+        yield return screenFader.StartFadeOut();
+        //OnLoadEnd?.Invoke();
 
-      //yield return new WaitForSeconds(0.5f);
+        //yield return new WaitForSeconds(0.5f);
 
-      isLoading = false;
+        IsLoading = false;
     }
 
     private IEnumerator StartupFade()
     {
-      //OnLoadBegin?.Invoke();
-      //yield return screenFader.StartFadeIn();
-      //yield return StartCoroutine(UnloadCurrent());
+        //OnLoadBegin?.Invoke();
+        //yield return screenFader.StartFadeIn();
+        //yield return StartCoroutine(UnloadCurrent());
 
-      // Fake wait time
-      //yield return new WaitForSeconds(1.0f);
+        // Fake wait time
+        //yield return new WaitForSeconds(1.0f);
 
-     // yield return StartCoroutine(LoadNew(sceneName));
-      yield return screenFader.FirstFadeUp();
-      //OnLoadEnd?.Invoke();
+        // yield return StartCoroutine(LoadNew(sceneName));
+        yield return screenFader.FirstFadeUp();
+        //OnLoadEnd?.Invoke();
 
-      //yield return new WaitForSeconds(0.5f);
+        //yield return new WaitForSeconds(0.5f);
 
-      //isLoading = false;
+        //isLoading = false;
     }
 
     private IEnumerator UnloadCurrent()
     {
-      AsyncOperation unloadOperation = SceneManager.UnloadSceneAsync(SceneManager.GetActiveScene());
+        AsyncOperation unloadOperation = SceneManager.UnloadSceneAsync(SceneManager.GetActiveScene());
 
-      //Can add the progress loading bar here for async progress
+        //Can add the progress loading bar here for async progress
 
-      while(!unloadOperation.isDone)
-        yield return null;
+        while (!unloadOperation.isDone)
+        {
+            yield return null;
+        }
     }
 
     private IEnumerator LoadNew(string sceneName)
     {
-		  AsyncOperation loadOperation = SceneManager.LoadSceneAsync(sceneName, LoadSceneMode.Additive);
-      
-      while(!loadOperation.isDone)
-        yield return null;
+        AsyncOperation loadOperation = SceneManager.LoadSceneAsync(sceneName, LoadSceneMode.Additive);
+
+        while (!loadOperation.isDone)
+        {
+            yield return null;
+        }
     }
 
     private void SetActiveScene(Scene scene, LoadSceneMode mode)
@@ -139,44 +143,44 @@ public class SceneLoader : Singleton<SceneLoader>
 
     private void ClearPlayerPrefPanelHistory()
     {
-      // Save player prefs in temp placeholder incase the app has just paused
-      deletedSavedCurrentPanel = PlayerPrefs.GetString("SavedCurrentPanel");
-      deletedSavedPanelHistoryCount = PlayerPrefs.GetInt("SavedPlayerHistoryCount");
-      for(int i = 0; i < deletedSavedPanelHistoryCount; i++)
+        // Save player prefs in temp placeholder incase the app has just paused
+        deletedSavedCurrentPanel = PlayerPrefs.GetString("SavedCurrentPanel");
+        deletedSavedPanelHistoryCount = PlayerPrefs.GetInt("SavedPlayerHistoryCount");
+        for (int i = 0; i < deletedSavedPanelHistoryCount; i++)
         {
-          string panelString = PlayerPrefs.GetString("SavedPanelHistory" + i);
-          deletedSavedPanelHistory.Add(panelString);
+            string panelString = PlayerPrefs.GetString("SavedPanelHistory" + i);
+            deletedSavedPanelHistory.Add(panelString);
         }
-      deleteShowPostLikert = PlayerPrefs.GetInt("ShowPostLikert");
+        deleteShowPostLikert = PlayerPrefs.GetInt("ShowPostLikert");
 
-      // Delete player prefs
-      PlayerPrefs.DeleteKey("SavedCurrentPanel");
-      PlayerPrefs.DeleteKey("SavedPanelHistory");
-      PlayerPrefs.DeleteKey("SavedPanelHistoryCount");
-      PlayerPrefs.DeleteKey("ShowPostLikert");
-      Debug.Log("History Cleared from palyer prefs in scene loader script");
+        // Delete player prefs
+        PlayerPrefs.DeleteKey("SavedCurrentPanel");
+        PlayerPrefs.DeleteKey("SavedPanelHistory");
+        PlayerPrefs.DeleteKey("SavedPanelHistoryCount");
+        PlayerPrefs.DeleteKey("ShowPostLikert");
+        Debug.Log("History Cleared from palyer prefs in scene loader script");
     }
 
     private void ReloadPlayerPrefPanelHistory()
     {
-      PlayerPrefs.SetString("SavedCurrentPanel", deletedSavedCurrentPanel);
-      PlayerPrefs.SetInt("SavedPlayerHistoryCount", deletedSavedPanelHistoryCount);
-      for(int i = 0; i < deletedSavedPanelHistoryCount; i++)
-      {
-          string panelString = deletedSavedPanelHistory[i];
-          PlayerPrefs.SetString("SavedPanelHistory" + i, panelString);
-      }
-      PlayerPrefs.SetInt("ShowPostLikert", deleteShowPostLikert);
+        PlayerPrefs.SetString("SavedCurrentPanel", deletedSavedCurrentPanel);
+        PlayerPrefs.SetInt("SavedPlayerHistoryCount", deletedSavedPanelHistoryCount);
+        for (int i = 0; i < deletedSavedPanelHistoryCount; i++)
+        {
+            string panelString = deletedSavedPanelHistory[i];
+            PlayerPrefs.SetString("SavedPanelHistory" + i, panelString);
+        }
+        PlayerPrefs.SetInt("ShowPostLikert", deleteShowPostLikert);
 
-    deletedSavedCurrentPanel = "";
-    deletedSavedPanelHistoryCount = 0;
-    deletedSavedPanelHistory.Clear();
-    deleteShowPostLikert = 0;
+        deletedSavedCurrentPanel = "";
+        deletedSavedPanelHistoryCount = 0;
+        deletedSavedPanelHistory.Clear();
+        deleteShowPostLikert = 0;
 
     }
 
-    
 
-    
+
+
 }
 
